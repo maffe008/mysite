2d97bdfce6af6a72040c82d1f708910bfa53c110
Maffee <maffe008@hotmail.com>
2016-12-08 21:58:00 +0800

    0.2.0|optionBar

---
+++ marcgis/static/js/demo.js
@@ -0,0 +1,50 @@
+function NC_onclick(){
+    NC_choose = document.getElementById("NC_choose");
+    if (NC_choose.className == "glyphicon glyphicon-unchecked"){
+        NC_choose.className ='glyphicon glyphicon-check';
+    }else if(NC_choose.className == "glyphicon glyphicon-check"){
+        NC_choose.className ='glyphicon glyphicon-unchecked';
+    }else{
+        alert("NC_choose ERROR");
+    }
+}
+function CIR_onclick(){
+    NC_choose = document.getElementById("CIR_choose");
+    if (NC_choose.className == "glyphicon glyphicon-unchecked"){
+        NC_choose.className ='glyphicon glyphicon-check';
+    }else if(NC_choose.className == "glyphicon glyphicon-check"){
+        NC_choose.className ='glyphicon glyphicon-unchecked';
+    }else{
+        alert("CIR_choose ERROR");
+    }
+}
+function NDVI_onclick(){
+    NC_choose = document.getElementById("NDVI_choose");
+    if (NC_choose.className == "glyphicon glyphicon-unchecked"){
+        NC_choose.className ='glyphicon glyphicon-check';
+    }else if(NC_choose.className == "glyphicon glyphicon-check"){
+        NC_choose.className ='glyphicon glyphicon-unchecked';
+    }else{
+        alert("NDVI_choose ERROR");
+    }
+}
+function Thermal_onclick(){
+    NC_choose = document.getElementById("Thermal_choose");
+    if (NC_choose.className == "glyphicon glyphicon-unchecked"){
+        NC_choose.className ='glyphicon glyphicon-check';
+    }else if(NC_choose.className == "glyphicon glyphicon-check"){
+        NC_choose.className ='glyphicon glyphicon-unchecked';
+    }else{
+        alert("Thermal_choose ERROR");
+    }
+}
+function DSM_onclick(){
+    NC_choose = document.getElementById("DSM_choose");
+    if (NC_choose.className == "glyphicon glyphicon-unchecked"){
+        NC_choose.className ='glyphicon glyphicon-check';
+    }else if(NC_choose.className == "glyphicon glyphicon-check"){
+        NC_choose.className ='glyphicon glyphicon-unchecked';
+    }else{
+        alert("DSM_choose ERROR");
+    }
+}
